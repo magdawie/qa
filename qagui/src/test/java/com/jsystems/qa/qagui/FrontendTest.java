2b0e3d0d22980b17cbd2d959a1cfdb9f0caab0d3
magdawie <magd.wieczorek@gmail.com>
2019-11-20 12:37:39 +0100

    nowe page

--- qagui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
+++ qagui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
@@ -1,5 +1,6 @@
 package com.jsystems.qa.qagui;
 
+import com.jsystems.qa.qagui.page.MainWordpressPage;
 import com.systems.qa.qagui.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,13 @@ public class FrontendTest extends ConfigFrontend {
     @Test
     public void FrontTest() {
         driver.get(Configuration.BASE_URL);
-        WebElement textElement_1 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(1)"));
-        String text1 = textElement_1.getText();
+     //   WebElement textElement_1 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(1)"));
+        MainWordpressPage mainWordpressPage = new MainWordpressPage(driver);
+        String text1 = mainWordpressPage.textElement_1.getText();
         assertTrue(text1.equals("WordPress powers"));
 
-        WebElement textElement_2 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(2)"));
-        String text2 = textElement_2.getText();
+      //  WebElement textElement_2 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(2)"));
+        String text2 = mainWordpressPage.textElement_2.getText();
         assertTrue(text2.contains("% of the internet."));
         assertThat(text2).matches("\\d+(% of the internet.)");
     }
---
+++ qagui/src/test/java/com/jsystems/qa/qagui/page/BasaePage.java
@@ -0,0 +1,12 @@
+package com.jsystems.qa.qagui.page;
+import org.openqa.selenium.WebDriver;
+
+    public class BasaePage{
+        public WebDriver driver;
+
+        public BasaePage(WebDriver driver) {
+            this.driver = driver;
+        }
+    }
+
+
