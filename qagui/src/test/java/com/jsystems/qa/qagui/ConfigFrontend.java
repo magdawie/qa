f43fa2248234ad33509e040da3f60baa0496ac9c
magdawie <magd.wieczorek@gmail.com>
2019-11-20 10:18:33 +0100

    front, skopiowany

--- qagui/src/test/java/com/jsystems/qa/qagui/ConfigFrontend.java
+++ qagui/src/test/java/com/jsystems/qa/qagui/ConfigFrontend.java
@@ -7,25 +7,73 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 public class ConfigFrontend {
     protected WebDriver driver;
 
-    @BeforeAll
-    public static void setupAll(){
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
+    //    String chromePath;
+    String fireFoxPath;
+    {
+        try {
+//            chromePath = Paths.get(getClass().getClassLoader().getResource("driver/chromedriver.exe")
+//                    .toURI()).toFile().getAbsolutePath();
+            fireFoxPath = Paths.get(getClass().getClassLoader().getResource("driver/geckodriver.exe")
+                    .toURI()).toFile().getAbsolutePath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
+
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.firefoxdriver().setup();
+    }
+
     @BeforeEach
-    public void setUpEch() {
+    public void setUpEach() throws MalformedURLException {
+        setupSystemProperties();
+
         driver = new FirefoxDriver();
+
+        setupDriver();
+    }
+
+    private void setupSystemProperties() {
+//        System.setProperty("webdriver.chrome.driver", chromePath);
+        System.setProperty("webdriver.gecko.driver", fireFoxPath);
+    }
+
+    private void setupDriver() {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
     }
+
     @AfterEach
     public void tearDownEach() {
         driver.quit();
+    }
+
+    private void setUpRemote() {
+        //        DesiredCapabilities cap = DesiredCapabilities.chrome();
+//        cap.setPlatform(Platform.LINUX);
+//        cap.setVersion("");
+//
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.setCapability("platform", "LINUX");
+//        options.setCapability("browserName", "chrome");
+//        driver = null;
+//        try {
+//            driver = new RemoteWebDriver(new URL("http://10.0.75.1:4444/wd/hub"), cap);
+//        setupDriver();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
